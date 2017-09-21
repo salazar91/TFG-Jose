defc486573293399b1d0234a21868f7f9c91c3e3
salazar91 <salazar912008@gmail.com>
2017-09-21 17:18:26 +0200

    Programas de prueba
     Programas para probar pillow, y la craeación de directorios. No entiendo porque el programa de pillow no me deja importar PIL si en la consola me deja. Me da fallo que no existe el modulo PIL.

---
+++ Programas/Probando pillow.py
@@ -0,0 +1,11 @@
+#from PIL import Image
+#import  PIL as pillow
+#from PIL import Image
+#import  PIL
+from __future__ import print_function
+import os, sys
+from PIL import Image
+
+#from PIL import Image
+print ('Hola')
+
